53fd2077ee6e04685ce67d4afa804897ff3accf2
facu2279 <2279@holbertonschool.com>
2021-05-02 23:29:44 +0000

    fix

--- prueba.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Write a script that starts a Flask web application """
-from flask import Flask, render_template
-from models import storage
-from models.state import State
-
-
-list_states = storage.all(State).values()
-print(list_states)
