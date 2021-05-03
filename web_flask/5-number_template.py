4cdf8186f81bdd55b75c1fc6b06f3483fb7c4931
facu2279 <2279@holbertonschool.com>
2021-05-03 00:50:04 +0000

    Fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application """
+""" Made by Facundo Diaz for Holberton School 2021 """
 from flask import Flask, render_template
 
 
@@ -9,38 +9,38 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_world():
-    """ Returns Hello HBNB """
+    """ Returns a specific string """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def HBNB_world():
-    """ Returns HBNB """
+    """ Returns a specific string """
     return "HBNB"
 
 
 @app.route('/c/<text>')
 def C_world(text):
-    """ Returns something with C """
+    """ Returns all with C """
     return 'C %s' % text.replace('_', ' ')
 
 
 @app.route('/python/<text>')
 @app.route('/python')
 def Python_world(text='is cool'):
-    """ Returns something with Python """
+    """ Returns all with Python """
     return 'Python %s' % text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>')
 def integer_world(n):
-    """ Returns something with number """
+    """ Returns all with number """
     return ("{} is a number".format(n))
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    """ Returns number in template """
+    """ Returns number in html document """
     return render_template('5-number.html', number=n)
 
 if __name__ == "__main__":
