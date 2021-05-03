38b6b99220d239c48f6da306f02ad2de6298935f
facu2279 <2279@holbertonschool.com>
2021-05-03 00:58:50 +0000

    Fix

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application """
+""" Made by Facundo Diaz for Holberton School 2021 """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -10,64 +10,64 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_world():
-    """ Returns Hello HBNB """
+    """ Returns a specific string """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb')
 def HBNB_world():
-    """ Returns HBNB """
+    """ Returns a specific string """
     return "HBNB"
 
 
 @app.route('/c/<text>')
 def C_world(text):
-    """ Returns something with C """
+    """ Returns all with C """
     return 'C %s' % text.replace('_', ' ')
 
 
 @app.route('/python/<text>')
 @app.route('/python')
 def Python_world(text='is cool'):
-    """ Returns something with Python """
+    """ Returns all with Python """
     return 'Python %s' % text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>')
 def integer_world(n):
-    """ Returns something with number """
+    """ Returns all with number """
     return ("{} is a number".format(n))
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    """ Returns number in template """
+    """ Returns number in a html document """
     return render_template('5-number.html', number=n)
 
 
 @app.route('/number_odd_or_even/<int:n>')
 def oddeven_template(n):
-    """ Returns number in template """
+    """ Returns number in a html page """
     return render_template('6-number_odd_or_even.html', number=n)
 
 
 @app.route('/states_list')
 def State_world():
-    """ List of states """
+    """ List of all states in the database """
     list_states = storage.all(State).values()
     return render_template('7-states_list.html', list_states=list_states)
 
 
 @app.route('/cities_by_states')
 def Cities_World():
-    """ List of cities from State """
+    """ List of cities from State in the database """
     list_states = storage.all(State).values()
     return render_template('8-cities_by_states.html', list_states=list_states)
 
 
 @app.teardown_appcontext
 def Down_World(exception):
-    """ DB will be closed automatically at the end of the request """
+    """ Killing the sql session """
     storage.close()
 
 
